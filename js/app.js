068df292e083987d96d500b69bcddb5fa27e687e
joel <joel_congo@hotmail.com>
2020-06-15 03:03:51 -0400

    add functions to calculate the circle diameters add function to update the map add function to add the political parties in a list draw the map by each political party base by year

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 (function () {
 
   const map = L.map('map', {
-    zoomSnap: .1,
-    // center: [12.169570,-68.990021],
     center: [12.1994847, -69.00883083333333],
-    // zoom: 10,
+    zoom: 11.5,
     zoomSnap: .2, // allow for smoother zooming
-    minZoom: 9,
+    minZoom: 11.5,
     maxZoom: 18,
     // maxBounds: L.latLngBounds([12.352131688081778, -69.1857147216797], [12.042131688081778, -68.72467714965526])
   });
@@ -19,33 +17,45 @@
     id: 'mapbox.dark',
     accessToken: accessToken
   }).addTo(map);
+
+
+
+  // use omnivore to load the CSV data
+  omnivore.csv('data/voting_data.csv')
+    // omnivore.geojson('data/voting-location_n.geojson')
+    .on('ready', function (e) {
+      // console.log(e.target)// inspect the output
+      // console.log(e.target.toGeoJSON())// inspect the output and compare the result with the above output
+      drawMap(e.target.toGeoJSON()); //pass the data as a geoJSON format to the caller drawMap
+      // drawLegend(e.target.toGeoJSON())
+    })
+    .on('error', function (e) {
+      // console.log(e.error[0].message);// inspect the output
+    }); // add the point data to the map
+
+
   // create Leaflet control for the legend
   const legendControl = L.control({
-    position: 'bottomright'
+    position: 'topright'
   });
 
-  // get coordinates on map click (temprary)
-  // map.on('click',function(e){
-  //   console.log(e.latlng);
-  // });
-
-
   // when the control is added to the map
-  // legendControl.onAdd = function (map) {
+  legendControl.onAdd = function (map) {
 
-  //   // select the legend using id attribute of legend
-  //   const legend = L.DomUtil.get("legend");
+    // select the legend using id attribute of legend
+    const legend = L.DomUtil.get("legend");
 
-  //   // disable scroll and click functionality 
-  //   L.DomEvent.disableScrollPropagation(legend);
-  //   L.DomEvent.disableClickPropagation(legend);
+    // disable scroll and click functionality 
+    L.DomEvent.disableScrollPropagation(legend);
+    L.DomEvent.disableClickPropagation(legend);
 
-  //   // return the selection
-  //   return legend;
+    // return the selection
+    return legend;
 
-  // }
+  }
+  legendControl.addTo(map); // add legend to the control
 
-  // legendControl.addTo(map);
+
 
   // create Leaflet control for the slider
   const sliderControl = L.control({
@@ -64,203 +74,232 @@
   }
 
   sliderControl.addTo(map);
-  // use omnivore to load the geojson data
-  omnivore.geojson('data/voting-data.geojson')
-    .on('ready', function (e) {
-      // console.log(e.target)// inspect the output
-      drawMap(e.target.toGeoJSON()); //pass the data as a geoJSON format to the caller drawMap
-      // drawLegend(e.target.toGeoJSON())
-    })
-    .on('error', function (e) {
-      // console.log(e.error[0].message);// inspect the output
-    }); // add the point data to the map
+
+
+  const years = [];
+  const polParties = {
+    "KDNT": "#C0C0C0",
+    "UKH": "#CC99FF",
+    "PIN": "#CCFFFF",
+    "PAR": "#FFFF00",
+    "MAN": "#3366FF",
+    "PNP": "#339966",
+    "FOL-PAN": "#FF9900",
+    "MFK": "#FFFFFF",
+    "MKP": "#808080",
+    "PS": "#FFCC99",
+    "KP": "#969696",
+    "MP": "#FF00FF",
+    "DP": "#E23F31",
+    "PRO-K": "#855418",
+    "PAIS": "#AADDE2",
+    "PAS": "#F0C136"
+  }
+
 
   function drawMap(data) {
-    var options = {
-      pointToLayer: function (feature, latlng) {
 
-        return L.circleMarker(latlng, {
-          opacity: 1,
-          weight: 2,
-          fillOpacity: 0
-        });
+    // console.log(data.features);
+    // for (var key in data.features[0].properties) {
 
+    //   if (key.includes("_" + $("#sliderVal").val()) && key != "sd_id") {
+    //     // console.log(key)
+    //     let politicaParty = key;
+    //     var options = {
+    //       pointToLayer: function (feature, latlng) {
+    //       //  console.log(feature['properties'][politicaParty]);// inspect the output
+    //         let partyColor = politicaParty.substr(0, politicaParty.indexOf('_'));
+    //         // console.log(partyColor);
+    //         return L.circleMarker(latlng, {
+    //           color: polParties[partyColor],
+    //           radius: calculateRadius(Number(feature['properties'][politicaParty])),
+    //           opacity: 1,
+    //           weight: 3,
+    //           fillOpacity: 0
+    //         });
+    //       },
+    //       onEachFeature:function(feature,layer)
+    //       {
+    //         layer.on("mouseover",function(){
+    //           console.log(layer.feature);
 
-      },
+    //         });
+    //       }
+    //     }
 
-    }
+    //     let l = L.geoJSON(data, options).addTo(map);
+    //   }
+    // }
+    updateMap(data, $("#sliderVal").val());
+    addPartyList(data, $("#sliderVal").val());
+    locationList(data);
+    sliderUI(data);
+  } // end of drawMap function
 
-    // create 2 separate layers from the geojson
-    let mfk = L.geoJSON(data, options).addTo(map);
-    let mkp = L.geoJSON(data, options).addTo(map);
-
-    mfk.setStyle({
-      color: '#6e77b0'
-    });
-
-    mkp.setStyle({
-      color: '#ff1a1a'
-    });
-
-
-    map.fitBounds(mfk.getBounds()); // get extent of the boys layer
-    map.setZoom(map.getZoom() - .4); // adjust the zoom level
-    resizeCircles(mfk, mkp, 2016); // Use 2016 temporary 
-
-    sequenceUI(mfk,mkp);
-
-  } // end of drawMap().
 
   function calculateRadius(val) {
 
     var radius = Math.sqrt(val / Math.PI); // calculate the radius
-    return radius * 2.5; // adjust the radius with .5 scale factor
+    return radius * 5; // adjust the radius with .5 scale factor
   } // end of calculateRadius()
 
-  function resizeCircles(electionLmfk, electionLmkp, currentYear) {
-    
-    electionLmfk.eachLayer(function (layer) {
-      // console.log(layer);
-      var r = calculateRadius(layer.feature.properties['MFK' + currentYear]);
-      layer.setRadius(r);
-    });
 
-    electionLmkp.eachLayer(function (layer) {
-      // console.log(layer);
-      var r = calculateRadius(layer.feature.properties['MKP' + currentYear]);
-      layer.setRadius(r);
-    });
+  function resizeCircles() {
 
-    retrieveInfo(electionLmfk, electionLmkp, currentYear); // call the retrieveInfo function to show the popup info
-    $('#year-display span').html(" " + currentYear);// show the current year
+
   } // end of resizeCircles()
 
-  function sequenceUI(electionLmfk, electionLmkp) {
+  function addPartyList(data, currentY) {
+    let parties = [];
+    // console.log(data.features);
+    data.features.forEach(feature => {
+      // console.log(feature.properties);
+      let name = Object.entries(feature.properties);
+      // console.log(name);
+      for (var i = 0; i < name.length; i++) {
+        if (i > 5) {
+
+          // console.log(name[i], feature);
+          let pol_party = name[i][0];
+          console.log(pol_party);
+          let split = pol_party.split('_');
+          let splitPolParty = split[0];
+          // let year = split[1];
+          // console.log(splitPolParty);
+          // console.log(pol_party,splitPolParty, year);
+          if (pol_party.includes(splitPolParty+'_'+currentY)) {
+            if (!parties.includes(splitPolParty)) {
+              parties.push(splitPolParty);
+            }
+
+          }
+
+        }
+      }
+    });
+    // console.log(parties);
+
+
+    // console.log(polParties[x], x);
+    for (let i = 0; i < parties.length; i++) {
+        // console.log(parties[i], i);
+      // $(".list-parties").append(`<li>${parties[i]}</li>`); 
+      for (var x in polParties) {
+        if (x == parties[i]) {
+          // console.log(parties[i], x);
+          // check for matched names
+          if (parties[i] == x) {
+            $(".list-parties").append(`<li style="background:${polParties[x]}">${parties[i]}</li>`); // create list
+          }
+
+        }
+
+      }
+
+
+    }
+
+  } // end of addPartyList function
+
+  function updateMap(data, currentYear) {
+    for (var key in data.features[0].properties) {
+
+      if (key.includes("_" + currentYear) && key != "sd_id") {
+        // console.log(key)
+        let politicaParty = key;
+        var options = {
+          pointToLayer: function (feature, latlng) {
+            //  console.log(feature['properties'][politicaParty]);// inspect the output
+            let partyColor = politicaParty.substr(0, politicaParty.indexOf('_'));
+            // console.log(partyColor);
+            return L.circleMarker(latlng, {
+              color: polParties[partyColor],
+              radius: calculateRadius(Number(feature['properties'][politicaParty])),
+              opacity: 1,
+              weight: 3,
+              fillOpacity: 0
+            });
+          },
+          onEachFeature: function (feature, layer) {
+            // layer.on("mouseover", function () {
+            //   console.log(layer.feature);
+
+            // }),
+            layer.on("click", function () {
+
+              retrieveInfo(layer, currentYear);
+            });
+          }
+        }
+
+        let l = L.geoJSON(data, options).addTo(map);
+      }
+    }
+
+    // let l = L.geoJSON(data, options).addTo(map);
+  } // end of updateMap function
+
+
+  function sliderUI(data) {
 
     $('#slider input[type=range]').on('input', function () {
       // console.log(this.value); // inspect the output
       var currentYear = this.value; // declare and assign this.value to the currentGrade
-      // $('#year-display span').html(currentYear);
-
-      resizeCircles(electionLmfk, electionLmkp, currentYear); // pass the arguments variables to the caller resizeCircle function.
-
-    })
-  } // end of sequenceUi()
-
-  function drawLegend(data) {
-
-  } // end of drawLegend()
-
-  function retrieveInfo(electionLmfk, currentYear) {
-
-    // select the element and reference with a variable
-    // and hide the element from view initially
-    var info = $('#info').hide();
-
-    // console.log(layer, currentGrade);
-    // on mouseover event, trigger layer selection and show the info window
-    electionLmfk.on("mouseover", function (e) {
-      info.show();
-      // console.log(e.layer.feature.properties);
-      // declare variable props and assign the layer properties object to it 
-      var props = e.layer.feature.properties;
-      console.log(props);
-      // console.log(Object.entries(props));
-      Object.entries(props).forEach(name => {
-        if (name[0].includes("MFK")) {
-
-          let n = name[0].replace(/[0-9]/g, ''); // remove years from the string
-          console.log(props);
-          console.log(n);
-          $('#info h3').html(`${props.Location}</br>`);
-          $('#info p.mfk span').html(`${n}: ${props.MFK2016}</br>`);
-          // $('#info p.mkp span').html(`${n}: ${props.MKP2016}</br>`);
-
-        }
-        if (name[0].includes("MKP")) {
-
-          let n = name[0].replace(/[0-9]/g, ''); // remove years from the string
-          console.log(props);
-          console.log(n);
-          $('#info h3').html(`${props.Location}</br>`);
-          // $('#info p.mfk span').html(`${n}: ${props.MFK2016}</br>`);
-          $('#info p.mkp span').html(`${n}: ${props.MKP2016}</br>`);
-
-        }
-      });
-
-      e.layer.setStyle({
-        fillOpacity: .6
-      });
-    });
-
-    // on mouseout hide the info window and reset the selection
-    electionLmfk.on("mouseout", function (e) {
-      info.hide();
-      e.layer.setStyle({
-        fillOpacity: 0
-      });
-    });
-
-
-    // On window resize unset any position properties
-    $(window).resize(function () {
-
-      info.css({
-        "left": "unset",
-        "right": "unset",
-        "top": "unset"
-      }); 
+      // resizeCircles(girlsLayer, boysLayer, currentYear); // pass the arguments variables to the caller resizeCircle function.
+      updateMap(data, currentYear);
     })
 
-    // // when the mouse moves on the document
-    $(document).mousemove(function (e) {
+  } // end of sliderUI function
 
-      //   // Check document size, if less than 800...
-      if ($(document).width() < 800) {
+  function locationList(data) {
+    // console.log(data.features);
+    for (var x in data.features) {
+      // console.log(data.features[x]);
+      let props = data.features[x].properties;
+      // console.log(props.Location);
+      $(".location-list").append(`<li class="location_item" id="loc_${props.sd_id}">${props.sd_id} ${props.Location}</li>`); // create list
+    }
+    $(".location_item").on("mouseover", function (e) {
 
-        // ...position the info window in the upper-right corner.
-        info.css({
-          "right": 10,
-          "top": 10,
-        });
+      $("#" + e.target.id).css("background-color", "green");
+      $("#" + e.target.id).css("cursor", "pointer");
+      //  let splitItemId=e.target.id.split("_");
 
-      } else {
 
-        // console.log($(document).width() )
-        // first offset from the mouse position of the info window
-        info.css({
-          "left": e.pageX + 6,
-          "margin-top": e.pageY - info.height() - 25
-        });
-
-        // console.log(info.offset().top, $(document).height()); // inspect the output
-        // if it crashes into the top, flip it lower right
-        if (info.offset().top < 4) {
-          info.css({
-            "margin-top": e.pageY + 15
-          });
-        }
-        // console.log(info.offset().left); // inspect the output
-        // if it crashes into the right, flip it to the left
-        if (info.offset().left + info.width() >= $(document).width() - 40) {
-          info.css({
-            "left": e.pageX - info.width() - 80
-          });
-        }
-      }
     });
+
+    $(".location_item").on("mouseout", function (e) {
+
+      // console.log($("#"+e.target.id).html());
+      $("#" + e.target.id).css("background-color", "#1E1E1E");
+
+    });
+
+
+  } // end of locationList function
+
+  function retrieveInfo(data, currentY) {
+
+    console.log(data.feature.properties, currentY);
+    inf = `<div class='infoL'><h6>${data.feature.properties.Location}</h6><p></p></div>`;
+    // for (var i in data.feature.properties) {
+    //   if (i.includes(currentY)) {
+    //     console.log(i,data.feature.properties[i]);
+    //     $(".infoL p").append("test");
+    //   }
+    // }
+
+
+    data.bindPopup(inf).openPopup();
+    // let inf=`<div>
+    // ${data.feature.properties.Location}
+
+    // ${data.feature.properties["MFK_"+$("#sliderVal").val()]}
+
+
+    // </div>`;
+
 
   }
 
-
-  // $('#gb-icon').on('click', function () {
-  //   let isOpen = $("#legend-container").css("display");
-  //   if (isOpen == "none") {
-  //     $("#legend-container").css("display", "block");
-
-  //   } else if (isOpen == "block") {
-  //     $("#legend-container").css("display", "none");
-  //   }
-  // });
 })();
